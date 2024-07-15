cb3c3bfc97ffede17082f47b3929a933bf846035
Luffyzinxjs <140118867+DavizinxDoJS@users.noreply.github.com>
2024-07-15 12:08:03 -0300

    Update databse.js

--- scripts-experimental/databse.js
+++ scripts-experimental/databse.js
@@ -1,42 +1,49 @@
 import { world } from "@minecraft/server";
 
 // Função para obter a base de dados usando um ID
-export function get(id) {
-    const dbData = world.getDynamicProperty(id);
+export function get(id, player = null) {
+    const dbData = player 
+        ? player.getDynamicProperty(id)
+        : world.getDynamicProperty(id);
     return dbData ? JSON.parse(dbData) : {};
 }
 
 // Função para salvar a base de dados usando um ID
-export function set(id, data) {
-    world.setDynamicProperty(id, JSON.stringify(data));
+export function set(id, data, player = null) {
+    const serializedData = JSON.stringify(data);
+    if (player) {
+        player.setDynamicProperty(id, serializedData);
+    } else {
+        world.setDynamicProperty(id, serializedData);
+    }
 }
 
 // Função para adicionar ou atualizar uma variável
-export function add(dbId, key, value) {
-    const db = get(dbId);
+export function add(dbId, key, value, player = null) {
+    const db = get(dbId, player);
     db[key] = value;
-    set(dbId, db);
+    set(dbId, db, player);
 }
 
 // Função para obter uma variável de um banco de dados
-export function getV(dbId, key) {
-    const db = get(dbId);
+export function getV(dbId, key, player = null) {
+    const db = get(dbId, player);
     return db[key];
 }
 
 // Função para remover uma variável de um banco de dados
-export function remove(dbId, key) {
-    const db = get(dbId);
+export function remove(dbId, key, player = null) {
+    const db = get(dbId, player);
     delete db[key];
-    set(dbId, db);
+    set(dbId, db, player);
 }
 
 // Função para limpar todas as variáveis de um banco de dados
-export function clear(dbId) {
-    set(dbId, {});
+export function clear(dbId, player = null) {
+    set(dbId, {}, player);
 }
 
 // Função para listar todas as variáveis de um banco de dados
-export function list(dbId) {
-    return get(dbId);
+export function list(dbId, player = null) {
+    return get(dbId, player);
 }
